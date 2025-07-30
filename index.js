327cfaf65f5f7db4e044dfc22a56a2120bd5bae7
Abhi Tiwari <114578515+thelunaticjester@users.noreply.github.com>
2025-07-30 16:00:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,91 @@
+// // // // // // // // // // const firstname="Amrit"
+// // // // // // // // // let age = 1;
+// // // // // // // // // // let favcolor = "blue";
+// // // // // // // // // // let height = 176;
+// // // // // // // // // // let favfood = "misal";
+// // // // // // // // // // if (favfood == "Pizza"){
+// // // // // // // // // //     console.log("favourite food found.");
+// // // // // // // // // // } else {
+// // // // // // // // // //     console.log ("favourite food not found.");
+// // // // // // // // // // }
+// // // // // // // // // let canGetaPassport =(age>=18)
+// // // // // // // // // console.log(canGetaPassport)
+// // // // // // // // function greet(name){ 
+// // // // // // // //     return "ram ram" + name;
+// // // // // // // // }
+// // // // // // // // let ans = greet(" Amrit ")
+// // // // // // // // console.log (ans)
+// // // // // // // function sum(a,b,c,d){
+// // // // // // //     let totalsum= a+b+c+d;
+// // // // // // //     return totalsum;
+// // // // // // // }
+// // // // // // // let ans1= sum("Amrit","harsh","raj","abhay");
+// // // // // // // let ans2 = sum("Abhi","fardeen","manik","divyansh");
+// // // // // // // console.log(ans1)
+// // // // // // // console.log(ans2)
+
+// // // // // // function cangetanaadhar(age){
+// // // // // //     if (age>=5){
+
+// // // // // //         return true;
+// // // // // //     }
+
+// // // // // //     else {
+// // // // // //         return false;
+// // // // // //     }
+// // // // // // }
+// // // // // function cangetanaadhar(age){
+// // // // //     if(age>=5){
+// // // // //         return true;
+// // // // //     } else{
+// // // // //         return false;
+// // // // //     }
+// // // // // }
+// // // // // let ans= cangetanaadhar(19)
+// // // // // console.log (ans);
+// // // // sum=0;
+// // // // n=5;
+// // // // for (let i=1;i<=n;i++){
+// // // // sum+=i;
+// // // // }
+// // // // console.log(sum)
+// // // let user ={
+// // //     name: "amrit",
+// // //     age: 24
+// // // }
+// // // console.log("namaste"   +  user.name  +  user.age)
+// // function greet (user){
+// //     console.log(" hi I am " + user.name + " and I am  " + user.age + " and I am a " + user.gender); 
+
+// //      if (user.age>=18){
+// //         console.log (" which can vote");
+
+// //     } else {
+// //         console.log(" sorry cannot vote");
+// //     }
+
+// // }
+// // let user ={
+// //     name: "amrit",
+// //     age: 21,
+// //     gender: "Male",
+// // } 
+// // greet (user);
+// function sumArray(numbers) {
+//   let sum = 0;
+//   for (let num of numbers) {
+//     sum += num;
+//   }
+//   return sum;
+// }
+// function sumArray(numbers) {
+//   let sum = 0;
+//   for (let num of numbers) {
+//     sum += num;
+//   }
+//   return sum;
+// }
+
+const a=1;
+const b=2;
+console.log(a+b);
