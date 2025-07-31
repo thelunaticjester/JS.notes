d016ee1a9a1bfc29dd220a1c229b56cab78108f8
Abhi Tiwari <114578515+thelunaticjester@users.noreply.github.com>
2025-07-31 14:56:45 +0530

    Add files via upload

---
+++ 02_variable.js
@@ -0,0 +1,6 @@
+let customerName ="Amrit"
+let customerID="a2101"
+const transactionID="QPR#21321"
+let consumerCity="Mumbai"
+console.table ([customerName,customerID,transactionID]);
+// prefer not to use var because of issue in block and functional scope
