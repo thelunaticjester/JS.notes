4c8d57e75e8647498b2c097af8fa605462970040
Abhi Tiwari <114578515+thelunaticjester@users.noreply.github.com>
2025-07-31 14:57:49 +0530

    Add files via upload

---
+++ 03_datatypes.js
@@ -0,0 +1,12 @@
+//these are all Primitive datatypes
+"use string"; // after using this syntax the whole code base will be treated as the newer version of js standard
+// alert (3+3) == we use node js or browser and for using the alert we use diffrent syntax for alert in js
+console.log (3+3); console.log("amrit") //don't write code this way as its not recommended and its not readable, the best war to write it by giving proper spaces in between them and make code readability high
+let name = "amrit"; //its a string data type as its quoted in "" (this. is how it shoud be written or used) , '' (not this one)
+let age = 18; //this datatype is used for numeric value.
+let loggedIn = true/false; // in this we use boolean datatype.
+let bigint // we use this if the nuber is greater than numeric type or number type which is 2 to the power 53.
+let userrollNumber = null; // its a stand alone value which is a representaion of empty value
+let state; // this represents the datatype which is undefined( means not defined)
+console.log(typeof null); // it behaves like object as it gives its type as object
+console.log(undefined); //it behaves and gives undefined as its type
