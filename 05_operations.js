fc46a2a99447ebff0a89f482c7d95423eff3d89d
Abhi Tiwari <114578515+thelunaticjester@users.noreply.github.com>
2025-08-02 18:04:17 +0530

    Add files via upload

---
+++ 05_operations.js
@@ -0,0 +1,28 @@
+// let value =3;
+// let negValue= -value; // here -value will be using value as an i\p and giving the o\p for console.log
+// console.log(negValue); // o/p = -3
+ 
+
+// console.log(2**3; == 2 to the power 3
+// console.log (3/2); == used for dividing
+// console.log (3%2); == used for checking reminder
+
+// for joining string we use
+//  let str1 = "amrit";
+//  let str2 = "  rai "
+
+//  let str3 =  str1+ str2;
+// console.log(str3); // in this the variable str3 is called and gives o\p which has combination of str1 and str2\\
+
+//  let gameCounter = 99;
+//  gameCounter++; // it'll add a numeric value (+1) into the game counter {the precedece operator added after the gameCounter fn is called postfix and if it would have been added before the game Counter it would have been called prefix}
+
+//{result = count++	result gets old value, then count increases.      	5	6
+
+//result = ++count	count increases first, then result gets new value	6	6}
+
+//  console.log(gameCounter); // the o/p will be 100 as 99 was the initial value and 1 is added to the gameCounter after ++ operation\\
+ 
+ 
+
+ 
