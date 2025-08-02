766ed4c4358a6af262836a455ec1da9ec4c805fe
Abhi Tiwari <114578515+thelunaticjester@users.noreply.github.com>
2025-08-02 18:03:58 +0530

    Add files via upload

---
+++ 04_conversions of datatypes.js
@@ -0,0 +1,80 @@
+// let score= 34;
+// console.log(typeof(score));
+// // or
+// console.log (typeof score); //in this type it defines about the type and gives response which is "number"
+
+// let valueinNumber= Number(score);
+// console.log(valueinNumber)
+// console.log(typeof (valueinNumber)); // it will give us the type of the value given in this case it was numeric so it gave number; if it would have been in double quotes ("") it would have given string and so on some examples are given below//
+
+
+// let score = "34gkdmf";
+ 
+// console.log (typeof score);
+// console.log (typeof(score)); // these can be used simultaneously for knowing the type of the variable given
+
+// let valueinNumber = Number (score);
+// console.log(valueinNumber);// it will show NaN ("not a number" )as its also a types and it shows this because js isn't scrictly checked.
+
+
+//  if it would have been null
+// let score = null;
+ 
+// console.log (typeof score);
+// console.log (typeof(score)); // these can be used simultaneously for knowing the type of the variable given
+
+// let valueinNumber = Number (score);
+// console.log(valueinNumber); // it would be given 0 which isn't completely correct ut not completely wrong
+
+
+// if it would have been for undefined
+// let score = undefined;
+ 
+// console.log (typeof score);
+// console.log (typeof(score)); // these can be used simultaneously for knowing the type of the variable given
+
+// let valueinNumber = Number (score);
+// console.log(valueinNumber); // it will also give NaN rather than giving undefined
+
+
+// if it would have been for boolean
+// let score = true;
+ 
+// console.log (typeof score);
+// console.log (typeof(score)); // these can be used simultaneously for knowing the type of the variable given
+
+// let valueinNumber = Number (score);
+// console.log(valueinNumber); // it will give 1 if true and 0 if false
+
+// let score = "amrit";
+// console.log(score); // o/p == amrit
+// console.log(typeof score); o/p == string
+// console.log(typeof(score));o/p == string
+// let valueinNumber = Number(score);
+// console.log(valueinNumber); // it'll give NaN coz the string isn't a number in rest below console.log it'll give nuber because its taking number from the function and its not strictly checked.
+// console.log(typeof valueinNumber); o/p == number
+// console.log(typeof(valueinNumber));o/p == number
+
+// let isLoggedIn= "";
+// console.log (isLoggedIn);// its an empty string and it prints an empty space coz the string which was given was empty; so it does print things but its not visible as it was an empty string
+
+// let BooleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof BooleanisLoggedIn);// o/p == it gives boolean as type
+// console.log(BooleanisLoggedIn); // this goives false as we gave it an empty string; if we would have given anything in that string it would have given true as response//
+ 
+
+// SUMMARY
+//33 == we get number as type in return 
+// "33" == we get sting as atype in return
+// "33anjnf" == we get string as a type in return but after console.log the main value we get NaN as the type
+// {for boolean ; true== 1 ; false == 0;
+// "" == false ; "we" == true} \\
+// these are basic conversions which are used in js\\
+
+
+// let someRandomNumber=33;
+//  console.log (someRandomNumber); // o\p == 33
+
+//  let StringinNumber = String (someRandomNumber); // the string fn before (someRandomNumber) is called constructor or conversion function.
+//  console.log (typeof StringinNumber); // o\p == string (as the string is the coversion function even tho the input no. is a numeric value )
+
