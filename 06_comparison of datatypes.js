058044b678b6d349b1bd7d2b4429454062288d23
Abhi Tiwari <114578515+thelunaticjester@users.noreply.github.com>
2025-08-03 22:41:19 +0530

    Add files via upload

---
+++ 06_comparison of datatypes.js
@@ -0,0 +1,17 @@
+//the most easy conversions are 
+
+// console.log(2 > 1);//o\p == true; as 2>1
+// console.log(2 >= 1);// o\p == true; as 2>1 but != 1 (! represents not)
+// console.log(2 == 1);// o\p == false; as 2!=1
+// console.log(2 != 1);//o/p == true; as 2!=1
+// console.log("2">2);// o\p == false ; as js isn't strictly typed so it coverts the string into numeric datatype unlike typescript.
+// console.log( null == 0 );false, it shows true because equall ops work diffrently and they don't convert null into a numeric datatype.
+// console.log( null > 0 );false
+// console.log( null >= 0 );true {it shows true because comparisons work diffrently, it js engine converts null into a no. and then it thins 0 and null is equal so thats why it shows true}
+// console.log( null === 0 );false 
+//console.log( undefined == 0); false { undefined will always give u false as it work diffrently than the null}.
+// we use === for strictly checking the type, knowing the datatype as it also compares type too and then give the result like "2"===2; it will give false as an ans co the type are not similar one is string and the other  one is numeric.
+
+
+
+
