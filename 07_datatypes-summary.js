5cdcaa7106fd12a94fb3c2cf1571d46ce9e230e5
Abhi Tiwari <114578515+thelunaticjester@users.noreply.github.com>
2025-08-04 01:26:10 +0530

    Add files via upload

---
+++ 07_datatypes-summary.js
@@ -0,0 +1,14 @@
+// there are 2 types of data types in js -> 1) Primitive datatype ; 2) Non-Primitive datatype
+
+
+
+//primitibe are of 7 types and they are call by value type and jo bhoi data hai wo direct refrence original memory ka nhi dete hai but refrence ko copy krke dete hai aur changes copy mein hi hote hai -> String, Number, Boolean , Null , Undefined , Symbol (use kiya jata hai to make value unique) , BigInt (used for denoting scientific value which are bigger in size)
+
+
+
+//Non-primitiove (refrence) datatype -> isme memory mein rerence allocate kiya ja skta hai no need for copying and all
+// types of non-primitive -> Array, objects(most imp topic), functions 
+
+
+// const score = 1000; \\ in this we don't tell  js that whether this is a string or a number as js is a dynamically typed language( dynamically typed = type checking happens at runtime(When the program actually running) and you don't have to declare the variable's type and it can change as the program is running. In this typed error go unnoticed until the program is running, ehich sime times makes debuging harder. And it is bit more slower as compared to statically typed.)
+// const scoreVal = 100.3; \\ decimal doesn't hacve any thing special value in js like float value.
