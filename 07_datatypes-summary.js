7e4423289001532d28a6ce1f28602e0324f9e994
Abhi Tiwari <114578515+thelunaticjester@users.noreply.github.com>
2025-08-03 22:41:49 +0530

    Add files via upload

---
+++ 07_datatypes-summary.js
@@ -0,0 +1,10 @@
+// there are 2 types of data types in js -> 1) Primitive datatype ; 2) Non-Primitive datatype
+
+
+
+//primitibe are of 7 types and they are call by value type and jo bhoi data hai wo direct refrence original memory ka nhi dete hai but refrence ko copy krke dete hai aur changes copy mein hi hote hai -> String, Number, Boolean , Null , Undefined , Symbol (use kiya jata hai to make value unique) , BigInt (used for denoting scientific value which are bigger in size)
+
+
+
+//Non-primitiove (refrence) datatype -> isme memory mein rerence allocate kiya ja skta hai no need for copying and all
+// types of non-primitive -> Array, objects(most imp topic), functions 
