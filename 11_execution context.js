44f97efe306d0c5daef5eed426ba7f9b59cd773b
Abhi Tiwari <114578515+thelunaticjester@users.noreply.github.com>
2025-08-05 02:44:49 +0530

    Delete 11_execution context.js

--- 11_execution context.js
+++
@@ -1,9 +0,0 @@
-// ye organised set of information hoti hai.. 
-// kuch informatiion ka collection hai jo properly organise krke mtlb ek data structure ka use kiya hoga
-//  and ye chunk of info kuch memory ko occypy krega and jis memory ke section ko ye occupy krke rkhega that section == execution context
-// its of 2 type ->
-
-// 1 -> Global execution context :- ye jab js ka prog start krte hai to ye create hota hai and ye stack mein aa jata hai and stay krta hai jab tak prog end nhi hota
-
-
-//2 -> Function execution context :- ye jab function run krte hai stack mein tab create hota hai 
