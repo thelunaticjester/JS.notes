79d13b4d1bd6b5118ad16af44fb1e12631e3b077
Abhi Tiwari <114578515+thelunaticjester@users.noreply.github.com>
2025-08-06 22:04:21 +0530

    Add files via upload

---
+++ 13_garbage collector.js
@@ -0,0 +1,4 @@
+// jo bhi memory blocks heap mein bnte hai dynamically wo release hotio hai with the help of garbage collector
+
+// garbage collector wo hai jo memory ab use nhi hone wali aage usse heap se js with the help of garbage collection process garbage collector ki help se release kr deta hai isse hum automatic mem management kehte hai.
+// isme sirf heap ka auto mem management hota hai 
