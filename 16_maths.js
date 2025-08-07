0bdc48877218bebf8749c0859e043c9654b74026
Abhi Tiwari <114578515+thelunaticjester@users.noreply.github.com>
2025-08-08 01:45:37 +0530

    Add files via upload

---
+++ 16_maths.js
@@ -0,0 +1,160 @@
+// // maths ki library by default aati hai js ke sath pehle se hi
+// //Math apne aap mein pehle se hi ek object hai
+
+//neeche saare fn diye hai jo .Maths ya Maths ke sath use krte hai
+
+
+// E
+// : 
+// 2.718281828459045
+// LN2
+// : 
+// 0.6931471805599453
+// LN10
+// : 
+// 2.302585092994046
+// LOG2E: 1.4426950408889634
+// LOG10E : 0.342944819032518
+// PI:3.141592653589793
+
+// SQRT1_2
+// : 
+// 0.7071067811865476
+
+// SQRT2
+// : 
+// 1.4142135623730951
+
+// abs
+// : 
+// ƒ abs()
+// acos
+// : 
+// ƒ acos()
+// acosh
+// : 
+// ƒ acosh()
+// asin
+// : 
+// ƒ asin()
+// asinh
+// : 
+// ƒ asinh()
+// atan
+// : 
+// ƒ atan()
+// atan2
+// : 
+// ƒ atan2()
+// atanh
+// : 
+// ƒ atanh()
+// cbrt
+// : 
+// ƒ cbrt()
+// ceil
+// : 
+// ƒ ceil()
+// clz32
+// : 
+// ƒ clz32()
+// cos
+// : 
+// ƒ cos()
+// cosh
+// : 
+// ƒ cosh()
+// exp
+// : 
+// ƒ exp()
+// expm1
+// : 
+// ƒ expm1()
+// f16round
+// : 
+// ƒ f16round()
+// floor
+// : 
+// ƒ floor()
+// fround
+// : 
+// ƒ fround()
+// hypot
+// : 
+// ƒ hypot()
+// imul
+// : 
+// ƒ imul()
+// log
+// : 
+// ƒ log()
+// log1p
+// : 
+// ƒ log1p()
+// log2
+// : 
+// ƒ log2()
+// log10
+// : 
+// ƒ log10()
+// max
+// : 
+// ƒ max()
+// min
+// : 
+// ƒ min()
+// pow
+// : 
+// ƒ pow()
+// random
+// : 
+// ƒ random()
+// round
+// : 
+// ƒ round()
+// sign
+// : 
+// ƒ sign()
+// sin
+// : 
+// ƒ sin()
+// sinh
+// : 
+// ƒ sinh()
+// sqrt
+// : 
+// ƒ sqrt()
+// tan
+// : 
+// ƒ tan()
+// tanh
+// : 
+// ƒ tanh()
+// trunc
+// : 
+// ƒ trunc()
+// Symbol(Symbol.toStringTag)
+// : 
+// "Math"
+// [[Prototype]]
+// : 
+// Object
+
+
+
+const max = 100
+const min = 200
+console.log(Math.ceil(Math.random()* (max-min+1)+min));//o\p is 106 its usually imp as random takes any value and then other eqn follows
+console.log(Math.abs(-4)); //o\p is 4
+console.log(Math.random());// o\p is 0.5265720022175888
+console.log(Math.min(4,2,10,9,3,4)); //o\p is 2
+console.log(Math.max(8,7,6,2,0,9));// o\p is 9
+console.log(Math.ceil(6.89));//o\p is 7
+console.log(Math.floor(8.72));// o\p is 8
+console.log(Math.round(5.62));// o\p is 6
+
+
+
+
+
+
