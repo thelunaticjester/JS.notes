ed6e6a50fe25cf0a0c0619fc5593265882a4ac83
Abhi Tiwari <114578515+thelunaticjester@users.noreply.github.com>
2025-08-19 23:13:42 +0530

    Add files via upload

---
+++ 23_api.js
@@ -0,0 +1,32 @@
+// api ka mtln apna kaam dusre ke sir mad dena ya daal dena.. just like mikey jaise wo mere sar dalta hai kaam ya aryan bhiaya
+
+// api = values aati hai backend se and hm usko kaise manage krte hai wo hogi api usecase or using
+
+
+// JSON aisa dikhta hai -  isme keys bhi string hoti hai and value bhi string hoti hai bss number direct aate hai like original and boooleans bhi baaki sab string ki form mein aate hai
+
+// {
+//     "name ": "abhi",
+//     "learning" : "api in js",
+
+
+// }
+
+
+// api mein hme kbhi kbhi arrays bhi mil skti hai aisa koi certain nhi hai ki object hi milega arrays ke andar object or loops use krna pd  skta hai like
+
+[
+
+
+    {},
+
+    {},
+
+    {},
+
+    {},
+
+
+]
+
+// ye saari api json format mein hi hai and ususally aise hi use hoti hai json ek js object notation hai (full form) ye std format hai api bhejne ka
