5be727adc31459ac35d6f71a99487e085c7eb48d
Abhi Tiwari <114578515+thelunaticjester@users.noreply.github.com>
2025-09-02 00:10:41 +0530

    Add files via upload

---
+++ 42_exercises_for_loops.js
@@ -0,0 +1,102 @@
+// 1-5 tak ke no. print krane hai and usko ek var add mein store kra ke print krane hai
+
+
+// let add = 0
+// let i = 1 
+// while (i <= 5){
+//     sum += 1;
+//     i++
+// }
+// console.log(add);
+
+// Write a function called sum that finds the sum from 1 to a number
+
+// function sum(n) {
+//     let sum = 0
+//     for (let i = 0; i <=n; i++) {
+//         sum+=i;
+        
+//     }return sum;
+// }
+// console.log(sum (15));
+
+// ek array countdown mein store krana hai 5 to 1 values
+
+// let countdown = []
+// let i = 5
+// while (i>=1) {
+//     countdown.push(i)
+//     sum-=i;
+//     i--;
+    
+// }
+// console.log(countdown);
+
+// do while ka use krna hai for making user to enter their fav tea until they enter "stop". aur usko tea collection named array mein store krna hai.
+
+// let teaCollection = []
+// let tea ; 
+// do {
+//     tea = prompt (`these are my fav(say "stop" to finish) `)
+//     if (tea !== "stop")
+//         teaCollection.push(tea);
+
+// } while (tea !== "stop");
+
+// do while ka use krke store krani hai value from 1-3 in  variable named total
+
+
+// let total = 0
+// let i = 1
+// do {
+//     total += i;
+//     i++;
+// } while (i<=3);
+
+// for loop likhne hai ,array elements ko multiply krna hai 2 se  and usko store krana hai multipliedNumbers naam ki array mein.
+
+// let multipliedNumbers = []
+// let randomNum = [2,4,6]
+// for (let i = 0; i < randomNum.length; i++) {
+//     totalNums= randomNum[i]* 2;
+//     multipliedNumbers.push (totalNums)
+    
+// }
+// console.log(multipliedNumbers);
+
+
+//for loop lagana hai jo list kre all cities ko  array mein and saari cities list ho in cities list naam ki array mein
+
+// let cities = ["paris","london","milan","madrid"]
+// let cityList = []
+// for (let  i= 0;  i< cities.length; i++) {
+//     const citytovisit = cities[i];
+//     cityList.push(citytovisit);
+// }
+// console.log(cityList);
+
+
+function sayHi (){
+    alert ("hello")
+}
+window.sayHi()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
