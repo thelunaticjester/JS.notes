e1b035d53fb4ea39030bed501ab5367809627e4b
Abhi Tiwari <114578515+thelunaticjester@users.noreply.github.com>
2025-09-18 04:44:00 +0530

    Update promise-async-await.ts

--- promise-async-await.ts
+++ promise-async-await.ts
@@ -1,3 +1,7 @@
+// we can only await inside an async function
+//we can await a function if it returns a promise
+//fn ke aage promise use krna = use await bnana
+
 async function main(){
     try{
         const data = await fetch('https://jsonplaceholder.typicoe.com/todos/1')
